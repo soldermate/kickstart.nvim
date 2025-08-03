0c63ae043da4064dd1620b3147e2160f49870b0c
Cameron Carslake <cscarslake@gmail.com>
2025-08-03 20:24:35 +0930

    change colortheme to tomorrow nigh

--- lua/custom/plugins/colour-theme.lua
+++ lua/custom/plugins/colour-theme.lua
@@ -1,55 +1,67 @@
+-- Using lazy.nvim
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
+  'deparr/tairiki.nvim',
+  lazy = false,
+  priority = 1000, -- recommended if you use tairiki as your default theme
 
   config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha',
-      background = {
-        light = 'latte',
-        dark = 'mocha',
+    require('tairiki').setup {
+      palette = 'dark', -- available options: dark, light, dimmed, tomorrow, light_legacy
+      default_dark = 'dark',
+      default_light = 'light',
+      transparent = false, -- don't set background colors
+      terminal = false, -- override nvim terminal colors
+      end_of_buffer = false, -- show end of buffer filler lines (tildes)
+      visual_bold = false, -- bolden visual selections
+      cmp_itemkind_reverse = false, -- reverse fg/bg on nvim-cmp item kinds
+
+      diagnostics = {
+        darker = false, -- darken diagnostic virtual text
+        background = true, -- add background to diagnostic virtual text
+        undercurl = false, -- use undercurls for inline diagnostics
       },
-      transparent_background = false,
-      show_end_of_buffer = false,
-      term_colors = false,
-      dim_inactive = {
-        enabled = false,
-        shade = 'dark',
-        percentage = 0.15,
-      },
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
-      styles = {
-        comments = { 'italic' },
-        conditionals = { 'italic' },
-        loops = {},
-        functions = {},
+
+      -- style for different syntactic tokens
+      -- see :help nvim_set_hl() for available keys
+      code_style = {
+        comments = { italic = true },
+        conditionals = {},
         keywords = {},
+        functions = {},
         strings = {},
         variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
+        parameters = {},
         types = {},
-        operators = {},
       },
-      color_overrides = {},
-      custom_highlights = {},
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
+
+      -- lualine theme config
+      lualine = {
+        transparent = true, -- remove background from center section
       },
+
+      -- which plugins to enable
+      plugins = {
+        all = false, -- enable all supported plugins
+        none = false, -- ONLY set groups listed in :help highlight-groups (see lua/tairiki/groups/neovim.lua). Manually enabled plugins will also be ignored
+        auto = true, -- auto detect installed plugins, currently lazy.nvim only
+
+        -- or enable/disable plugins manually
+        -- see lua/tairiki/groups/init.lua for the full list of available plugins
+        -- either the key or value from the M.plugins table can be used as the key here
+        --
+        -- setting a specific plugin manually overrides `all` and `auto`
+        -- treesitter = true,
+        -- semantic_tokens = true,
+      },
+
+      -- optional function to modify or add colors to the palette
+      -- palette definitions are in lua/tairiki/palette
+      colors = function(colors, opts) end,
+
+      -- optional function to override highlight groups
+      highlights = function(groups, colors, opts) end,
     }
 
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'tairiki'
   end,
 }
