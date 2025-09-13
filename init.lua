c979dd81cca84f2d3f4a5b04fa1d2ade46325155
Cameron Carslake <cscarslake@protonmail.com>
2025-09-13 20:06:52 +0930

    add html and css lsp

--- init.lua
+++ init.lua
@@ -496,19 +496,16 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
-        -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        clangd = {},
+        pyright = {},
         ts_ls = {},
-        --
-
+        cssls = {
+          cmd = { 'vscode-css-language-server' },
+          filetypes = { 'css' },
+          init_options = { provideFormatter = true },
+        },
+        html = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -525,12 +522,6 @@ require('lazy').setup({
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
       require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
@@ -776,6 +767,7 @@ require('lazy').setup({
         'python',
         'javascript',
         'typescript',
+        'css',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
